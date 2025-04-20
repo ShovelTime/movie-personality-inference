b76ef5d74e19e1e3438198f56610e5c1df38d1f8
Gousaid67 <gousaid67@gmail.com>
2025-04-20 13:35:38 +0200

    personality inference through an OpenAI endpoint

--- preprocessing/main.py
+++ preprocessing/main.py
@@ -1,5 +1,23 @@
 import json
 import sys
+import pandas as pd
+from openai import OpenAI
+
+class PersonalityMatrix:
+    def __init__(self):
+        self.open = 0.0
+        self.extra = 0.0
+        self.agree = 0.0
+        self.consc = 0.0
+        self.neuro = 0.0
+
+    def input_values(vals):
+        self.open = vals[0]
+        self.extra = vals[1]
+        self.agree = vals[2]
+        self.consc = vals[3]
+        self.neuro = vals[4]
+
 
 class Reviewer:
     def __init__(self):
@@ -7,12 +25,27 @@ class Reviewer:
         self.movies_watched = dict()
         self.reviewTexts = ""
 
-def ten_core_filter_pass(user_map, movie_map):
+
+def parse_completion(completion):
+    res_arr = array('f')
+    str_arr = completion.split(',')
+    for r_string in str_arr:
+        s_split = r_string.split(':')[1]
+        res_arr.append(s_split)
+
+    res = PersonalityMatrix()
+    res.input_values(res_arr)
+    return res
+
+
+
+
+def k_core_filter_pass(user_map, movie_map, k):
     pending_removal = list()
     filtered = False
     for movie_id in movie_map.keys():
         movie = movie_map[movie_id]
-        if len(movie) < 10:
+        if len(movie) < k:
             pending_removal.append(movie_id)
             filtered = True
             for user_id in movie:
@@ -30,12 +63,12 @@ def ten_core_filter_pass(user_map, movie_map):
     pending_removal.clear()
 
     for user in user_map.values():
-        if len(user.movies_watched) < 10:
+        if len(user.movies_watched) < k:
             filtered = True
             pending_removal.append(user.reviewerID)
     for user_id in pending_removal:
         user = user_map[user_id]
-        for movie_id in user.movies_watched:
+        for movie_id in user.movies_watched.keys():
             movie = movie_map[movie_id]
             movie.remove(user_id)
         del user_map[user_id]
@@ -75,14 +108,52 @@ def main():
         print("Finished parsing reviews, user count = " + str(len(user_map)) + "\nmovie count:" + str(len(movie_map)))
         
         #10-core filtering
-        while ten_core_filter_pass(user_map, movie_map) != False:
+        while k_core_filter_pass(user_map, movie_map, 25) != False:
             continue
 
 
-        print("\n\n Processed 10-core filter passes, user count = " + str(len(user_map)) + "\nmovie count:" + str(len(movie_map)))
+        print("\n\nProcessed 25-core filter passes, user count = " + str(len(user_map)) + "\nmovie count:" + str(len(movie_map)))
 
-        input("Press Enter to Continue....")
-        return 0
+#    personality_map = dict()
+
+    #OpenAI
+#    openai_key = "EMPTY"
+#    openai_base = "http://localhost:8000/v1"
+
+#    openai_client = OpenAI(
+#        api_key=openai_key,
+#        base_url=openai_base,
+#    )
+
+#    prompt = "You are a psychologist experienced in analysizing text and determining their personality profile. Your task will be to classify user text based on the OCEAN model, or Big Five personality traits model. From the text, you want to determine five traits: \nOpenness: How open a person is to new experiences, and to allowing their imagination to run wild. \nExtraversion: How energetic and socially outgoing a person, tending to be more talkative and assertive in conversation.\nAgreeableness: A person who exhibits prosocial behavior, including trust, kindness and affection.\nConscientiousness: A person who has high level of thoughtfulness, self-control, focused on a goal and organized.\nNeuroticism: A person who shoes moody behaviour, sadness, and unstable emotions, generally is negative.\nYou will classify a user's text based on these five personalities traits. for each trait, assign a score on a scale from 1 to 7, with a score of 1 meaning that the text does not align with the trait's values, and 7 meaning that the text aligns extremely well with the trait's values. Values can be rounded up to a tenth, like 4.5 for example. \n Analyze the following text based on the Big Five personality traits, and output the scores without any extra texts or explanation using the following format \"Openness: score, Extraversion: score,  Agreeableness: score, Conscientiousness: score, Neuroticism: score\":"
+
+#    print("Beginning personality inference...")
+
+#    for user in user_map.values():
+#        userID = user.reviewerID
+#        input = user.reviewTexts
+#        res = openai_client.completions.create(
+#            model="unsloth/DeepSeek-R1-Distill-Llama-8B-unsloth-bnb-4bit",
+#            messages=[
+#               {"role": "system", "content": prompt},
+#                {"role": "user", "content": input},
+#            ]
+#        )
+#        personality_m = parse_completion(res)
+#        personality_map[userID] = personality_m
+
+    print("Writing excel to /tmp/dataset")
+    user_dataframe = pd.Dataframe(user_map)
+    movie_dataframe = pd.Dataframe(movie_map)
+
+    with pd.ExcelWriter(/tmp/dataset/out.xlsx) as excel_out:
+        user_dataframe.to_excel(excel_out, sheet_name="User")
+        movie_dataframe.to_excel(excel_out, shee_name="Movie")
+
+
+
+    input("Press Enter to Continue....")
+    return 0
 
 if __name__ == '__main__':
     sys.exit(main())
