eda244dcd67058aac4225ec879c3b83a5ec8319b
Gousaid67 <gousaid67@gmail.com>
2025-05-07 12:38:17 +0200

    use of new LM

--- preprocessing/main.py
+++ preprocessing/main.py
@@ -5,10 +5,12 @@ import re
 import pandas as pd
 from openpyxl.cell.cell import ILLEGAL_CHARACTERS_RE
 from openai import OpenAI
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
+import torch
 
-MAX_TOKEN_COUNT = 12880 * 0.70 # maximum token count that the model can read at once. Divided by 3 to allow space for reasoning
-PROMPT_TOKEN_COUNT = 422 # token space taken by the system prompt
-PROMPT = "You are a psychologist experienced in analysizing text and determining their personality profile. Your task will be to classify user text based on the OCEAN model, or Big Five personality traits model. From the text, you want to determine five traits: \nOpenness: How open a person is to new experiences, and to allowing their imagination to run wild. \nExtraversion: How energetic and socially outgoing a person, tending to be more talkative and assertive in conversation.\nAgreeableness: A person who exhibits prosocial behavior, including trust, kindness and affection.\nConscientiousness: A person who has high level of thoughtfulness, self-control, focused on a goal and organized.\nNeuroticism: A person who shoes moody behaviour, sadness, and unstable emotions, generally is negative.\nYou will classify a user's text based on these five personalities traits. for each trait, assign a score on a scale from 1 to 7, with a score of 1 meaning that the text does not align with the trait's values, and 7 meaning that the text aligns extremely well with the trait's values. Values can be rounded up to a tenth, like 4.5 for example. \n Analyze the following text based on the Big Five personality traits, and output the scores without any extra texts or explanation using the following format \"Openness: score, Extraversion: score,  Agreeableness: score, Conscientiousness: score, Neuroticism: score\":"
+#MAX_TOKEN_COUNT = 12880 * 0.70 # maximum token count that the model can read at once. Divided by 3 to allow space for reasoning
+#PROMPT_TOKEN_COUNT = 422 # token space taken by the system prompt
+#PROMPT = "You are a psychologist experienced in analysizing text and determining their personality profile. Your task will be to classify user text based on the OCEAN model, or Big Five personality traits model. From the text, you want to determine five traits: \nOpenness: How open a person is to new experiences, and to allowing their imagination to run wild. \nExtraversion: How energetic and socially outgoing a person, tending to be more talkative and assertive in conversation.\nAgreeableness: A person who exhibits prosocial behavior, including trust, kindness and affection.\nConscientiousness: A person who has high level of thoughtfulness, self-control, focused on a goal and organized.\nNeuroticism: A person who shoes moody behaviour, sadness, and unstable emotions, generally is negative.\nYou will classify a user's text based on these five personalities traits. for each trait, assign a score on a scale from 1 to 7, with a score of 1 meaning that the text does not align with the trait's values, and 7 meaning that the text aligns extremely well with the trait's values. Values can be rounded up to a tenth, like 4.5 for example. \n Analyze the following text based on the Big Five personality traits, and output the scores without any extra texts or explanation using the following format \"Openness: score, Extraversion: score,  Agreeableness: score, Conscientiousness: score, Neuroticism: score\":"
 
 class Reviewer:
     def __init__(self):
@@ -16,27 +18,26 @@ class Reviewer:
         self.movies_watched = dict()
         self.reviewTexts = list()
 
+trait_names = ["agreeableness", "openness", "conscientiousness", "extraversion", "neuroticism"]
 
 def parse_completion(completion):
     res = ["","","","",""]
-    str_arr = completion.split(',')
-    for r_string in str_arr:
-        split = r_string.split(':')
-        match split[0].strip():
-            case "Openness":
-                res[0] = split[1]
+    for trait, score in zip(trait_names, completion):
+        match trait.strip():
+            case "openness":
+                res[0] = score
                 continue
-            case "Extraversion":
-                res[1] = split[1]
+            case "extraversion":
+                res[1] = score
                 continue
-            case "Agreeablesness":
-                res[2] = split[1]
+            case "agreeablesness":
+                res[2] = score
                 continue
-            case "Conscientiousness":
-                res[3] = split[1]
+            case "conscientiousness":
+                res[3] = score
                 continue
-            case "Neuroticism":
-                res[4] = split[1]
+            case "neuroticism":
+                res[4] = score
                 continue
 
     return res
@@ -108,44 +109,72 @@ def k_core_filter_pass(user_map, movie_map, k):
 
     return filtered
 
-def perform_personality_inference(user_map, movie_map):
-        personality_map = map()
-        #OpenAI
-        openai_key = "EMPTY"
-        openai_base = "http://localhost:8000/v1"
-        openai_client = OpenAI(
-            api_key=openai_key,
-            base_url=openai_base,
-        )
+#def perform_personality_inference(user_map, movie_map):
+#        personality_map = map()
+#        #OpenAI
+#        openai_key = "EMPTY"
+#        openai_base = "http://localhost:8000/v1"
+#        openai_client = OpenAI(
+#            api_key=openai_key,
+#            base_url=openai_base,
+#        )
+#
+#        print("Beginning personality inference...")
+#
+#        for user in user_map.values():
+#            temp_list = list()
+#            user_id = user.reviewerID
+#            for input in user.reviewTexts:
+#                res = openai_client.chat.completions.create(
+#                    model="unsloth/DeepSeek-R1-Distill-Llama-8B-unsloth-bnb-4bit",
+#                    messages=[
+#                    {"role": "user", "content": PROMPT + "\n" + input},
+#                    ]
+#                )
+#                print("\nOutput:\n\n" + res.choices[0].message.content)
+#                temp_list.append(parse_completion(res.choices[0].message.content))
+#            final_personality = average_personality(temp_list)
+#            personality_map[user_id] = final_personality
+#
+#        print("Writing excel to /tmp/dataset")
+#
+#        movie_frame = decompose_movie_map(movie_map, user_map, personality_map)
+#        #user_frame = decompose_user_map(user_map) #, personality_m) work personality matrix in eventually
+#
+#        #user_dataframe = pd.DataFrame(personality_map, columns=["user_id", "openness", "extraversion", "agreeableness", "conscientiousness", "neuroticism"])
+#        movie_dataframe = pd.DataFrame(movie_frame, columns=["movie_id", "user_id", "score", "openness", "extraversion", "agreeableness", "conscientiousness", "neuroticism"])
+#
+#        with pd.ExcelWriter("/tmp/dataset/out.xlsx") as excel_out:
+#            user_dataframe.to_excel(excel_out, sheet_name="User")
+#            movie_dataframe.to_excel(excel_out, sheet_name="Movie")
 
-        print("Beginning personality inference...")
 
-        for user in user_map.values():
-            temp_list = list()
-            user_id = user.reviewerID
-            for input in user.reviewTexts:
-                res = openai_client.chat.completions.create(
-                    model="unsloth/DeepSeek-R1-Distill-Llama-8B-unsloth-bnb-4bit",
-                    messages=[
-                    {"role": "user", "content": PROMPT + "\n" + input},
-                    ]
-                )
-                print("\nOutput:\n\n" + res.choices[0].message.content)
-                temp_list.append(parse_completion(res.choices[0].message.content))
-            final_personality = average_personality(temp_list)
-            personality_map[user_id] = final_personality
+def perform_prediction(user_map):
+    warnings.filterwarnings('ignore')
+    model = AutoModelForSequenceClassification.from_pretrained("KevSun/Personality_LM", ignore_mismatched_sizes=True)
+    tokenizer = AutoTokenizer.from_pretrained("KevSun/Personality_LM")
 
-        print("Writing excel to /tmp/dataset")
+    personality_map = dict()
+    
+    print("Performing Prediction")
 
-        movie_frame = decompose_movie_map(movie_map, user_map, personality_map)
-        #user_frame = decompose_user_map(user_map) #, personality_m) work personality matrix in eventually
+    for user in user_map:
+        personality_list = list()
+        for review_string in user.reviewTexts:
+            tokenized = tokenizer(review_string, return_tensors='pt', padding=True, truncation=True, max_length=128)
+            model.eval()
+            
+            with torch.no_grad():
+                outputs = model(**tokenized) #Query model
 
-        #user_dataframe = pd.DataFrame(personality_map, columns=["user_id", "openness", "extraversion", "agreeableness", "conscientiousness", "neuroticism"])
-        movie_dataframe = pd.DataFrame(movie_frame, columns=["movie_id", "user_id", "score", "openness", "extraversion", "agreeableness", "conscientiousness", "neuroticism"])
+            predictions = torch.nn.functional.softmax(outputs.logits, dim=-1)
+            predicted_scores = predictions[0].tolist()
+            personality = parse_completion(predicted_scores)
+            personality_list.append(personality)
+        final_personality = personality_average(personality_list)
+        personality_map[user.reviewerID] = final_personality
 
-        with pd.ExcelWriter("/tmp/dataset/out.xlsx") as excel_out:
-            user_dataframe.to_excel(excel_out, sheet_name="User")
-            movie_dataframe.to_excel(excel_out, sheet_name="Movie")
+    return personality_map
 
 
 
@@ -184,9 +213,23 @@ def main():
         while k_core_filter_pass(user_map, movie_map, 20) != False:
             continue
 
-
         print("\n\nProcessed 20-core filter passes, user count = " + str(len(user_map)) + "\nmovie count:" + str(len(movie_map)))
 
+        personality_map = perform_prediction(user_map)
+
+        print("Writing excel to /tmp/dataset")
+
+        movie_frame = decompose_movie_map(movie_map, user_map, personality_map)
+        #user_frame = decompose_user_map(user_map) #, personality_m) work personality matrix in eventually
+
+        #user_dataframe = pd.DataFrame(personality_map, columns=["user_id", "openness", "extraversion", "agreeableness", "conscientiousness", "neuroticism"])
+        movie_dataframe = pd.DataFrame(movie_frame, columns=["movie_id", "user_id", "score", "openness", "extraversion", "agreeableness", "conscientiousness", "neuroticism"])
+
+        with pd.ExcelWriter("/tmp/dataset/out.xlsx") as excel_out:
+            #user_dataframe.to_excel(excel_out, sheet_name="User")
+            movie_dataframe.to_excel(excel_out, sheet_name="Movie")
+
+        
 
     #average = 0
     #highest = 0
